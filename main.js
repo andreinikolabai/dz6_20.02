566bcc666c24ebca8e444de578e8ed7fd9260e0b
andreinikolabai <andreiplus2000@gmail.com>
2023-02-21 16:07:11 +0200

    finish

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
 let hour = prompt('input number of hours', '');
 let res;
-if (hour <= 24) {
-    res = 3600 * hour;
-    alert('in ' + hour + ' hour - ' + res + ' sec');
-}
-else{
-    alert('error');
-}
-
+res = 3600 * hour;
+alert('in ' + hour + ' hour - ' + res + ' sec');
